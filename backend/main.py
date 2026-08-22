2f12f054469d41829827be943471fca47a980df1
Pranav Garg <317madhavgarg@gmail.com>
2026-08-22 15:39:17 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # Allow React frontend to communicate with FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
